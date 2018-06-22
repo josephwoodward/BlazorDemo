30a9c74a0db693bfd00dc7c7b00db4fabc325da7
Joseph Woodward <joseph.woodward@just-eat.com>
2018-06-22 18:46:19 +0100

    Updated

---
+++ demo1/ToDoComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace demo1
+{
+    public class ToDoComponent : BlazorComponent
+    {
+        public string NewItem { get; set; }
+
+        public IList<string> List { get; set; }
+
+        public bool ListIsInitialised => List != null;
+
+        protected override void OnInit()
+        {
+            List = new List<string>();
+        }
+        
+        public void AddItem()
+        {
+            List.Add(NewItem);
+            NewItem = string.Empty;
+        }
+    }
+}
